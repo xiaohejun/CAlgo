273a6a2781a304323e11c2e118c9877ae5bd0420
hejun <2931094367@qq.com>
2021-04-10 19:27:21 +0800

    添加public.h

---
+++ include/calgo_public.h
@@ -0,0 +1,19 @@
+#ifndef CALGO_PUBLIC_H
+#define CALGO_PUBLIC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define STATIC_CAST(type, val) ((type)(val))
+
+inline void *GetArraryVal(const void *arr, uint32_t index, size_t valSize);
+
+inline void *Swap(void *a, void *b, size_t valSize);
+
+inline void Assign(void *a, const void *b, size_t valSize);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
